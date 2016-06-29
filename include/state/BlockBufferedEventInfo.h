bc2fc734e0882d41e2b4600aa906fef315760ab0
godsme <darwin_yuan@hotmail.com>
2016-06-29 12:55:06 +0800

    added BlockBufferedEventInfo.h

---
+++ include/state/BlockBufferedEventInfo.h
@@ -0,0 +1,51 @@
+#ifndef HD5783E6C_4701_4324_9769_CAE7C24B8376
+#define HD5783E6C_4701_4324_9769_CAE7C24B8376
+
+#include <state/BufferedEventInfo.h>
+
+L4_NS_BEGIN
+
+struct BlockBufferedEventInfo: BufferedEventInfo
+{
+   BlockBufferedEventInfo( const TransStrategyDecisionMaker& strategy
+                         , const FailedRequestListener* listener);
+
+   OVERRIDE(const void* getMsg() const);
+   OVERRIDE(const void* getMsgBuffer() const);
+   OVERRIDE(size_t getMsgSize() const);
+
+   OVERRIDE(cub::Status saveEvent(const EventInfo& event));
+
+private:
+   cub::Status saveMsg(const EventInfo& event);
+
+private:
+   size_t size;
+
+private:
+   ABSTRACT(const void* getMsgBlock(size_t requiredSize) const);
+};
+
+///////////////////////////////////////////////////////////////////
+template <cub::U16 MSG_SIZE>
+struct GenericBlockBufferedEventInfo : BlockBufferedEventInfo
+{
+   GenericBlockBufferedEventInfo
+            ( const TransStrategyDecisionMaker& strategy
+            , const FailedRequestListener* listener)
+      : BlockBufferedEventInfo(strategy, listener)
+   {}
+
+private:
+   const void* getMsgBlock(size_t requiredSize) const
+   {
+      return requiredSize > sizeof(msg) ? 0 : msg;
+   }
+
+private:
+   cub::U8   msg[MSG_SIZE];
+};
+
+L4_NS_END
+
+#endif /* HD5783E6C_4701_4324_9769_CAE7C24B8376 */
---
+++ src/BlockBufferedEventInfo.cpp
@@ -0,0 +1,69 @@
+#include <cub/base/Assertions.h>
+#include <state/BlockBufferedEventInfo.h>
+#include <trans-dsl/TslStatus.h>
+#include <event/concept/InvalidMsgSize.h>
+
+#include <string.h>
+
+L4_NS_BEGIN
+
+using namespace cub;
+using namespace ev;
+
+//////////////////////////////////////////////////////////////////
+BlockBufferedEventInfo::BlockBufferedEventInfo
+     ( const TransStrategyDecisionMaker& strategy
+     , const FailedRequestListener* listener)
+   : BufferedEventInfo(&strategy, listener)
+   , size(INVALID_MSG_SIZE)
+{
+}
+
+//////////////////////////////////////////////////////////////////
+const void* BlockBufferedEventInfo::getMsg() const
+{
+   return getMsgBuffer();
+}
+
+//////////////////////////////////////////////////////////////////
+Status BlockBufferedEventInfo::saveEvent(const EventInfo& event)
+{
+   CUB_ASSERT_SUCC_CALL(saveMsg(event));
+   updateEventId(event.getEventId());
+   return TSL_SUCCESS;
+}
+
+//////////////////////////////////////////////////////////////////
+const void* BlockBufferedEventInfo::getMsgBuffer() const
+{
+   return getMsgBlock(size);
+}
+
+//////////////////////////////////////////////////////////////////
+size_t BlockBufferedEventInfo::getMsgSize() const
+{
+   return size;
+}
+
+//////////////////////////////////////////////////////////////////
+Status BlockBufferedEventInfo::saveMsg(const EventInfo& event)
+{
+   const void* const src = event.getMsg();
+   if (src == 0)
+   {
+      if(event.getMsgSize() != 0) return TSL_FAILED;
+
+      size = 0;
+      return TSL_SUCCESS;
+   }
+
+   const void* dest = getMsgBlock(event.getMsgSize());
+   CUB_ASSERT_VALID_PTR(dest);
+
+   size = event.getMsgSize();
+   ::memcpy((void*) dest, src, size);
+
+   return TSL_SUCCESS;
+}
+
+L4_NS_END
