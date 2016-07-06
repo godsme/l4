45baeffade2e0471d6b2e597de714fc231722590
godsme <darwin_yuan@hotmail.com>
2016-07-06 09:45:26 +0800

    modified FooAsyncAction2

--- tests/foo/src/l2-context/FooAsynAction2.cpp
+++ tests/foo/src/l2-context/FooAsynAction2.cpp
@@ -1,23 +1,27 @@
+#include <cub/log/log.h>
 #include <foo/l2-context/FooAsynAction2.h>
 #include <event/concept/Event.h>
 #include <trans-dsl/TslStatus.h>
 #include <foo/l4-sched/FooEvent.h>
-
-#include <iostream>
+#include <trans-dsl/sched/concept/TransactionInfo.h>
 
 L4_NS_BEGIN
 
 ///////////////////////////////////////////////////////////////////
-cub::Status FooAsynAction2::exec(const tsl::TransactionInfo&)
+cub::Status FooAsynAction2::exec(const tsl::TransactionInfo& info)
 {
+    DBG_LOG("start Foo Instance = %p", info.getInstanceId());
+
     WAIT_ON(EV_EVENT2, handleEvent2);
 
     return TSL_CONTINUE;
 }
 
 ///////////////////////////////////////////////////////////////////
-cub::Status FooAsynAction2::handleEvent2(const tsl::TransactionInfo&, const ev::Event& event)
+cub::Status FooAsynAction2::handleEvent2(const tsl::TransactionInfo& info, const ev::Event& event)
 {
+    DBG_LOG("handle event: Foo Instance = %p", info.getInstanceId());
+
     return TSL_SUCCESS;
 }
 
